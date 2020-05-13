65c5bd7f7b2f03839d04cca624783fd96f98f215
Ajay MT <ajay.tatachar@gmail.com>
2020-05-13 09:50:41 -0500

    don't highlight syntax

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pandoc --highlight-style=kate --toc -s -c style.css README.md -o index.html
-pandoc --highlight-style=kate --toc -s -c style.css syntax.md -o syntax.html
+pandoc --toc -s -c style.css README.md -o index.html
+pandoc --toc -s -c style.css syntax.md -o syntax.html
